fdc1ccfbfda682e9e088d16289b15fdb61b262b2
陈碧滔 <peter.chen@useful-person.com>
2020-04-29 02:50:41 +0800

    Updates

--- src/components/Background/Chessboard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import styles from './Chessboard.css'
-
-export default () => {
-  return <>
-    <div className={styles.chessboard}></div>
-  </>
-}
--- src/components/Border/MultipleBorder.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import styles from './MultipleBorder.css'
-
-export default () => {
-  return <>
-    <div className={styles.border}>多重边框</div>
-  </>
-}
